ce87c8a12227390c8d764569ac5788e87a1cd11d
germanskrobak <germanskrobak@gmail.com>
2015-03-08 08:55:16 -0300

    Avance :D

---
+++ 04-templates-engines/temp/template1.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+	var source=$("#entry-template").html();
+	var template=Handlebars.compile(source);
+
+		$.getJSON("data.json",function(data)
+		{
+
+			Handlebars.registerHelper('fullName',function (person) 
+			{
+				return person.firstName+" "+person.lastName;	
+			});	
+				$('body').append(template(data));
+		});
+	
+	//var data = $.getJSON({ 
+	//		url: 'data.json'
+	//	});
+	
+	/*data.done(function(profile)
+	{
+		var ctx = {};
+		ctx.data = profile;
+		var html= template(ctx);
+		
+		Handlebars.registerHelper('fullName',function (person) 
+		{
+			return person.firstName+" "+person.lastName;	
+		});	
+	
+		
+		$('body').append(template(html));
+	});
+*/
+	
+	
+
+});
